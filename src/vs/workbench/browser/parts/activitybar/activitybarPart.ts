e4d37ea846d43cc16e6c7a4b78acd276e1a39022
Mikhail Stotsik <mikhail.stotsik@softgamings.com>
2024-12-11 09:20:07 +0300

    Main Menu sidebar redesign

--- src/vs/workbench/browser/parts/activitybar/activitybarPart.ts
+++ src/vs/workbench/browser/parts/activitybar/activitybarPart.ts
@@ -41,7 +41,7 @@ import { SwitchCompositeViewAction } from '../compositeBarActions.js';
 
 export class ActivitybarPart extends Part {
 
-	static readonly ACTION_HEIGHT = 48;
+	static readonly ACTION_HEIGHT = 42;
 
 	static readonly pinnedViewContainersKey = 'workbench.activity.pinnedViewlets2';
 	static readonly placeholderViewContainersKey = 'workbench.activity.placeholderViewlets';
@@ -49,8 +49,8 @@ export class ActivitybarPart extends Part {
 
 	//#region IView
 
-	readonly minimumWidth: number = 48;
-	readonly maximumWidth: number = 48;
+	readonly minimumWidth: number = 42;
+	readonly maximumWidth: number = 42;
 	readonly minimumHeight: number = 0;
 	readonly maximumHeight: number = Number.POSITIVE_INFINITY;
 
